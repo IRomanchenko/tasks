10bb3af38af29978368280fabaf198d231405365
Igor Romanchenko <igor.a.romanchenko@gmail.com>
2019-07-03 11:06:08 +0300

    triangle task solution

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,10 +1,29 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class Task {
-    public static void main(String[] args) {
-        System.out.println("Hello");
 
+    public List<Integer> largestTriangle(List<Integer> arr) {
+
+        List<Integer> sorted = arr.stream()
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.toList());
+
+        for (int i = 0; i < sorted.size() - 2; i++) {
+            Integer a = sorted.get(i);
+            Integer b = sorted.get(i + 1);
+            Integer c = sorted.get(i + 2);
+            if (a < b + c) {
+                return Arrays.asList(a, b, c);
+            }
+        }
+
+        throw new IllegalArgumentException("The list has no possible triangles");
     }
 
-    public int someMetod() {
-        return 42;
-    }
+//    [1, 34, 4, 5, 3, 10, 100, 1]
+//      a + b > c, a + c > b, b + c > a 
+//    Perimeter = a + b + c  
+//    3, 4, 5 -> 12
+
 }
--- src/test/java/TaskTest.java
+++ src/test/java/TaskTest.java
@@ -1,12 +1,29 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class TaskTest {
     private Task task = new Task();
 
     @Test
-    void someMetod() {
-        assertEquals(42, task.someMetod());
+    void largestTriangle() {
+        List<Integer> result = task.largestTriangle(Arrays.asList(1, 34, 4, 5, 3, 10, 100, 1));
+        assertEquals(Arrays.asList(5,4,3), result);
+    }
+
+    @Test
+    void singleTriangle() {
+        List<Integer> result = task.largestTriangle(Arrays.asList(1, 1, 1));
+        assertEquals(Arrays.asList(1,1,1), result);
+    }
+
+    @Test
+    void noTriangle() {
+        assertThrows(IllegalArgumentException.class, () -> task.largestTriangle(Arrays.asList(1, 34, 4, 5, 1, 10, 100)));
     }
 }
